1fdbe8e2ae49b3f8e27b3b123a04dc24107d4818
KimKyengJun <juniair.kim@gmail.com>
2016-04-17 03:14:58 +0900

    replace file

--- lib/Log.h
+++ lib/Log.h
@@ -1,23 +1,26 @@
 #ifndef _LOG_CLASS_H_
 #define _LOG_CLASS_H_
-
+#include "Library.h"
+#include <ctime>
 namespace System {
-    #include "required_lib.h"
-    typedef char* TAG;
-    typedef char* MESSAGE;
+    
+    typedef std::string TAG;
+    typedef std::string MESSAGE;
 
     class Log {
-    private:
-        std::chrono::duration
-
-        void logToFile(TAG tag, MESSAGE msg) const {
-            File *file = fopen("log.txt")
-        }
-    public:
-        static void print(TAG tag, MESSAGE msg) const {
-            prtinf("%s: %s\n", tag, msg);
-            logToFile(tag, msg);
-        }
+		private:
+			
+			static void logToFile(TAG tag, MESSAGE msg) 
+			{
+				FILE *file = fopen("log.txt", "a+");
+					
+				fclose(file);
+			}
+		public:
+			static void print(TAG tag, MESSAGE msg) 
+			{
+				logToFile(tag, msg);
+			}
         
     };    
 };
