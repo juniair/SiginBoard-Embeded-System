29fbd595235aed33a60776a3f891eb86e16d3505
KimKyengJun <juniair.kim@gmail.com>
2016-04-17 03:14:45 +0900

    rename file

---
+++ lib/Library.h
@@ -0,0 +1,11 @@
+#ifndef _LIBRARY_H_
+#define _LIBRARY_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ISO C++14 standard library
+#include <iostream>
+#include <chrono>
+    
+#endif
